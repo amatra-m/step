d96d4bdf1377cce303afdd95f616621074dcd90b
amatra-m <amatramenon7@gmail.com>
2026-09-07 22:11:17 +0530

    Add files via upload

---
+++ FindMinimum.java
@@ -0,0 +1,35 @@
+public class FindMinimum {
+
+    public static int findMin(int[] nums) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+
+                // Minimum must be on the right side
+                left = mid + 1;
+
+            } else {
+
+                // Minimum may be mid or on the left side
+                right = mid;
+            }
+        }
+
+        return nums[left];
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+
+        int result = findMin(nums);
+
+        System.out.println("Minimum Element: " + result);
+    }
+}
